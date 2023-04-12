1948c9577c79614e61a0d97a1c702c04bdbd7a58
Гараев Адель <hajlan.adel@mail.ru>
2023-04-12 17:12:12 +0300

    Initial commit

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from main.models import User, Answer, Question, Test, TestingSystem, UserType, QuestionType, TestType
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Answer)
+admin.site.register(Question)
+admin.site.register(Test)
+admin.site.register(TestingSystem)
+admin.site.register(UserType)
+admin.site.register(QuestionType)
+admin.site.register(TestType)
