a94a1f750c23d307080eabdf74b2c7db961dbe1f
Гараев Адель <hajlan.adel@mail.ru>
2023-06-12 21:24:05 +0300

    Commit

--- manage.py
+++ manage.py
@@ -2,11 +2,15 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from dotenv import load_dotenv
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoRest.settings')
+
+    env_path = '/Users/adelgaraev/Desktop/3курсКурсач/djangoRest/keys.env'
+    load_dotenv(env_path)
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
